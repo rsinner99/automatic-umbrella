75e91a6c295105921c5a4db7d474947c73e20b50
Robin Sinner <robin.sinner@t-online.de>
2021-11-06 12:25:48 +0000

    Deactivate CSRF checks for perfomance testing. Made problems with Apache JMeter

--- web/web/settings.py
+++ web/web/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware', 
     'django.middleware.common.CommonMiddleware',
 #    'django.middleware.csrf.CsrfViewMiddleware',
+    'web.middleware.DisableCSRFMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
