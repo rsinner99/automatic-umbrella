f36e1e7eb5dad1771ae469a3bba05429b1cf4694
rsinner99 <robin.sinner@t-online.de>
2021-12-28 14:52:58 +0100

    Updated test

--- e2e_tests/test_run_script.py
+++ e2e_tests/test_run_script.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+
 from tracing_rca.testsuite.base import Runner
 
 logger = logging.getLogger("testrunners.selenium")
@@ -19,17 +20,19 @@ class RunScriptTestValid(Runner):
         chrome_options = Options()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-gpu")
         self.driver = webdriver.Chrome(options=chrome_options)
 
         self.driver.get("http://192.168.84.7/frontend/login")
-        self.driver.find_element_by_name('username').send_keys('test')
-        self.driver.find_element_by_name('password').send_keys('test1234test')
+        self.driver.find_element(By.NAME, 'username').send_keys('test')
+        self.driver.find_element(By.NAME, 'password').send_keys('test1234test')
         time.sleep(1)
-        self.driver.find_element_by_tag_name("button").click()
+        self.driver.find_element(By.TAG_NAME, "button").click()
         return super().setUp()
 
     def tearDown(self) -> None:
-        self.driver.close()
+        #self.driver.close()
         return super().tearDown()
 
     def test(self):
@@ -60,9 +63,8 @@ class RunScriptTestValid(Runner):
         self.driver.find_element_by_xpath("//input[@value='Save']").click()
 
         self.driver.find_element(By.LINK_TEXT, 'Peers').click()
-        lastRow = self.driver.findElement(By.xpath("(//table[1]/tbody/tr)[last()]"))
-        peer_id = lastRow.findlement(By.xpath("(//td[1]")).text
-
+        peer_id = self.driver.find_element(By.XPATH, "//table/tbody/tr[last()]/td[1]").text
+  
         # Creating a doc
         self.driver.find_element(By.LINK_TEXT, 'Docs').click()
         self.driver.find_element(By.LINK_TEXT, 'Add doc').click()
@@ -79,8 +81,7 @@ class RunScriptTestValid(Runner):
         self.driver.find_element_by_xpath("//input[@value='Save']").click()
 
         self.driver.find_element(By.LINK_TEXT, 'Docs').click()
-        lastRow = self.driver.findElement(By.xpath("(//table[1]/tbody/tr)[last()]"))
-        doc_id = lastRow.findlement(By.xpath("(//td[1]")).text
+        doc_id = self.driver.find_element(By.XPATH, "//table/tbody/tr[last()]/td[1]").text
 
         # Creating a task
         self.driver.find_element(By.LINK_TEXT, 'Tasks').click()
