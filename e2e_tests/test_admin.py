6d83e5ae120504a882ed320d69d50c923949a133
rsinner99 <robin.sinner@t-online.de>
2021-12-28 15:56:40 +0100

    New e2e tests & token auth

---
+++ e2e_tests/test_admin.py
@@ -0,0 +1,52 @@
+import logging
+import sys
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+from tracing_rca.testsuite.base import Runner
+
+logger = logging.getLogger("testrunners.selenium")
+logger.setLevel(logging.DEBUG)
+
+
+class CreateFileTestValid(Runner):
+    def setUp(self) -> None:
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+        self.driver.get("http://192.168.84.7/frontend/login")
+        self.driver.find_element(By.NAME, 'username').send_keys('root')
+        self.driver.find_element(By.NAME, 'password').send_keys('root')
+        time.sleep(1)
+        self.driver.find_element(By.TAG_NAME, "button").click()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.driver.get("http://192.168.84.7/frontend/")
+        self.driver.find_element(By.LINK_TEXT, 'Logout')
+        self.driver.close()
+        return super().tearDown()
+
+    def test(self):
+        logger.debug('Run_script test 1...')
+
+        self.driver.get("http://192.168.84.7/frontend/")
+        self.driver.find_element(By.LINK_TEXT, 'Admin').click()
+        assert "Site administration" in self.driver.title
+
+        
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr)
+    logging.getLogger("testrunners.selenium_base").setLevel(logging.DEBUG)
+    unittest.main()
---
+++ e2e_tests/test_ping_host.py
@@ -0,0 +1,60 @@
+import logging
+import sys
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+from tracing_rca.testsuite.base import Runner
+
+logger = logging.getLogger("testrunners.selenium")
+logger.setLevel(logging.DEBUG)
+
+
+class CreateFileTestValid(Runner):
+    def setUp(self) -> None:
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+        self.driver.get("http://192.168.84.7/frontend/login")
+        self.driver.find_element(By.NAME, 'username').send_keys('test')
+        self.driver.find_element(By.NAME, 'password').send_keys('test1234test')
+        time.sleep(1)
+        self.driver.find_element(By.TAG_NAME, "button").click()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.driver.get("http://192.168.84.7/frontend/")
+        self.driver.find_element(By.LINK_TEXT, 'Logout')
+        self.driver.close()
+        return super().tearDown()
+
+    def test(self):
+        logger.debug('Run_script test 1...')
+
+        self.driver.get("http://192.168.84.7/frontend/")
+        self.driver.find_element(By.LINK_TEXT, 'Tasks').click()
+        task_choice = Select(self.driver.find_element(By.ID, "id_taskname"))
+        task_choice.select_by_value("pinger.discover")
+
+        self.driver.find_element(By.ID, "discover_id_host").send_keys('172.17.0.0/30')
+        self.driver.find_element(By.ID, "discover_id_timeout").send_keys('1')
+
+        self.driver.find_element_by_xpath("//input[@value='Run task']").click()
+        time.sleep(10)
+        output = self.driver.find_element(By.NAME, "result").text
+        assert "172.17.0.1" in output
+        
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr)
+    logging.getLogger("testrunners.selenium_base").setLevel(logging.DEBUG)
+    unittest.main()
