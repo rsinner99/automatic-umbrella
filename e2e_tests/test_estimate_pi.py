9db1e73e09a8920738971870854c6d43d086b8c1
rsinner99 <robin.sinner@t-online.de>
2021-12-28 19:25:55 +0100

    New e2e-tests

--- e2e_tests/test_estimate_pi.py
+++ e2e_tests/test_estimate_pi.py
@@ -15,7 +15,7 @@ logger = logging.getLogger("testrunners.selenium")
 logger.setLevel(logging.DEBUG)
 
 
-class CreateFileTestValid(Runner):
+class EstimatePiTestValid(Runner):
     def setUp(self) -> None:
         chrome_options = Options()
         chrome_options.add_argument("--no-sandbox")
@@ -52,6 +52,43 @@ class CreateFileTestValid(Runner):
         output = self.driver.find_element(By.NAME, "result").text
         assert "3.1" in output
         
+
+class EstimatePiTestTimeout(Runner):
+    def setUp(self) -> None:
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+        self.driver.get("http://192.168.84.7/frontend/login")
+        self.driver.find_element(By.NAME, 'username').send_keys('test')
+        self.driver.find_element(By.NAME, 'password').send_keys('test1234test')
+        time.sleep(1)
+        self.driver.find_element(By.TAG_NAME, "button").click()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.driver.get("http://192.168.84.7/frontend/")
+        self.driver.find_element(By.LINK_TEXT, 'Logout')
+        self.driver.close()
+        return super().tearDown()
+
+    def test(self):
+        logger.debug('Run_script test 1...')
+
+        self.driver.get("http://192.168.84.7/frontend/")
+        self.driver.find_element(By.LINK_TEXT, 'Tasks').click()
+        task_choice = Select(self.driver.find_element(By.ID, "id_taskname"))
+        task_choice.select_by_value("cal.estimate_pi")
+
+        self.driver.find_element(By.ID, "estimate_pi_id_interval").send_keys('10000')
+
+        self.driver.find_element_by_xpath("//input[@value='Run task']").click()
+        time.sleep(10)
+        output = self.driver.find_element(By.NAME, "result").text
+        assert "3.1" in output
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr)
