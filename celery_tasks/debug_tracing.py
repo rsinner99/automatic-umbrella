320770f8984500d6edebf8279632dd60e48df6fd
Robin Sinner <robin.sinner@t-online.de>
2022-07-23 13:29:28 +0000

    Switch from opentracing to opentelemtry

--- celery_tasks/debug_tracing.py
+++ celery_tasks/debug_tracing.py
@@ -1,7 +1,6 @@
 import traceback
-import opentracing
-from opentracing import logs, tags
-from opentracing_instrumentation.request_context import get_current_span
+from opentelemetry import trace
+from opentelemetry.trace.propagation import get_current_span
 
 from worker import DEBUG
 
@@ -13,27 +12,27 @@ def trace_params(trace_all=False, new_span=False, **additional_params):
             if trace_all:
                 span = get_current_span()
                 if new_span:
-                    span = opentracing.tracer.start_span(func.__name__, child_of=span)
+                    tracer = trace.get_tracer_provider().get_tracer(__name__)
+                    span = tracer.start_span(func.__name__, child_of=span)
                 with span:
                     if len(args) > 0:
-                        span.set_tag('input.args', args)
+                        span.set_attribute('input.args', args)
                     for k, v in kwargs.items():
-                        span.set_tag('input.{}'.format(k), v)
+                        span.set_attribute('input.{}'.format(k), v)
                     for k, v in additional_params.items():
-                        span.set_tag(k, v)
+                        span.set_attribute(k, v)
             try:
                 result = func(*args, **kwargs)
                 if trace_all:
-                    span.set_tag('output', str(result))
+                    span.set_attribute('output', str(result))
                 return result
             except Exception as e:
-                span.set_tag(tags.ERROR, True)
-                span.log_kv({
-                    logs.EVENT: 'error',
-                    logs.ERROR_KIND: type(e),
-                    logs.ERROR_OBJECT: e,
-                    logs.MESSAGE: str(e),
-                    logs.STACK: traceback.format_exc()
+                span.add_event({
+                    "event": 'error',
+                    "kind": type(e),
+                    "object": e,
+                    "message": str(e),
+                    "stack": traceback.format_exc()
                 })
         return wrapper
     return decorator
--- celery_tasks/worker.py
+++ celery_tasks/worker.py
@@ -1,14 +1,14 @@
 import os
-from opentracing_instrumentation.client_hooks import celery as celery_hooks
-from opentracing_instrumentation.client_hooks import requests as requests_hooks
-from celery import Celery
-from celery.signals import worker_process_init, worker_process_shutdown
 
-from tracing import initialize_tracing, close_tracing
+from celery import Celery
+from celery.signals import worker_process_init
+
+from tracing import initialize_tracing
 import celery_settings
 
 DEBUG = True
 
+SERVICE_NAME = os.environ.get('SERVICE_NAME')
 BROKER_URL = celery_settings.BROKER_URL
 REDIS_URL = celery_settings.REDIS_URL
 
@@ -16,16 +16,9 @@ app = Celery('tasks', broker=BROKER_URL, backend=REDIS_URL)
 app.config_from_object(celery_settings, namespace='CELERY')
 app.autodiscover_tasks()
 
-celery_hooks.install_patches()
-requests_hooks.install_patches()
-
 
 # workaround: celery workers need to create a tracer instance on process initiation, 
 # otherwise no spans are reported
 @worker_process_init.connect(weak=False)
 def init_celery_tracing(*args, **kwargs):
-    initialize_tracing()
-
-@worker_process_shutdown.connect(weak=False)
-def close_celery_tracing(*args, **kwargs):
-    close_tracing
+    initialize_tracing(SERVICE_NAME)
